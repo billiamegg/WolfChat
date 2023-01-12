72519219ea275b243c9913323b15bbbd51e17bc5
billiamegg <122169777+billiamegg@users.noreply.github.com>
2023-01-12 16:59:31 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,9 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-    <a href="https://www.freecounterstat.com" title="hit counter"><img src="https://counter10.optistats.ovh/private/freecounterstat.php?c=2kmg4y3m6fsj3sqp5py8y66x9n7p4r55" border="0" title="hit counter" alt="hit counter"></a>
-
-<div id="sfc2kmg4y3m6fsj3sqp5py8y66x9n7p4r55"></div><script type="text/javascript" src="https://counter10.optistats.ovh/private/counter.js?c=2kmg4y3m6fsj3sqp5py8y66x9n7p4r55&down=async" async></script><br><a href="https://www.freecounterstat.com">hit counter</a><noscript><a href="https://www.freecounterstat.com" title="hit counter"><img src="https://counter10.optistats.ovh/private/freecounterstat.php?c=2kmg4y3m6fsj3sqp5py8y66x9n7p4r55" border="0" title="hit counter" alt="hit counter"></a></noscript>
-      </main>
+    <script type="text/javascript" src="https://www.counters-free.net/count/by9v"></script><br>
+ <a href='https://www.versicherungen.at/ablebensversicherung-rechner/'>Risikoversicherung</a> <script type='text/javascript' src='https://whomania.com/ctr?id=672e1b580734c3bc6c4e85e00f6907a5714d560a'></script>
+     </main>
 
       <Footer />
     </div>
